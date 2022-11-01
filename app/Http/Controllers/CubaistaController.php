ad73180862ecb1622d0be95f0e3c230d4d60a1f7
usadilsondigital <usadilsondigital@gmail.com>
2022-11-01 18:27:03 -0500

    Cubaista Controller

---
+++ app/Http/Controllers/CubaistaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cubaista;
+use App\Http\Requests\StoreCubaistaRequest;
+use App\Http\Requests\UpdateCubaistaRequest;
+
+class CubaistaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCubaistaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCubaistaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cubaista  $cubaista
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cubaista $cubaista)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cubaista  $cubaista
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cubaista $cubaista)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCubaistaRequest  $request
+     * @param  \App\Models\Cubaista  $cubaista
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCubaistaRequest $request, Cubaista $cubaista)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cubaista  $cubaista
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cubaista $cubaista)
+    {
+        //
+    }
+}

f10356437014b5622f16a91fe62bc58fdfdedbc4
usadilsondigital <usadilsondigital@gmail.com>
2022-11-02 22:01:47 -0500

    Providers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/{locale}/dashboard';
+    public const WELCOME = '/{locale}/welcome';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.

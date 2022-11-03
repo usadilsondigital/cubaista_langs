5093f281964ac93f8a91db0413ff97886dbcbbf8
usadilsondigital <usadilsondigital@gmail.com>
2022-11-02 21:03:00 -0500

    Migration Cubaista

--- database/migrations/2022_11_01_052336_create_cubaistas_table.php
+++ database/migrations/2022_11_01_052336_create_cubaistas_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('cubaistas', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('website')->nullable();
+            $table->string('company_name')->nullable();
+            $table->text('additional_notes')->nullable();
             $table->timestamps();
         });
     }

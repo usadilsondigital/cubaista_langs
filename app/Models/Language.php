3d9913eb3775fff05fbf29ba5c8560c271f0343d
usadilsondigital <usadilsondigital@gmail.com>
2022-11-02 22:01:34 -0500

    Language Model

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Language extends Model
 {
     use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'languages';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var bigint
+     */
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'code',
+        'english_name',
+        'directionality',
+        'local_name',
+        'url_wiki',
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'url_wiki' => 'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code',
+    ];
+
+    
 }

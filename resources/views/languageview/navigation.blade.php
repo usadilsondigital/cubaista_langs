9f95b9292deaa02a2ef81e9eeb8787e68909ff43
usadilsondigital <usadilsondigital@gmail.com>
2022-11-03 17:39:33 -0500

    Navigation Blades

--- resources/views/languageview/navigation.blade.php
+++ resources/views/languageview/navigation.blade.php
@@ -1,5 +1,11 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->  
+    <!-- Primary Navigation Menu -->
+    <div class="p-3 bg-primary text-white text-center" style="background-color: cornflowerblue">
+        <h3> {{ __('messages.you_love_cuba') }} {{ __('messages.or') }} 
+            {{ __('messages.think_you_might') }} {{ __('messages.so_you_are_a_cubaista') }} </h3>
+        <h4>{{ __('messages.and') }} {{ __('messages.we_are_your_dmc') }} <b style="color:navy">({{ __('messages.destination_management_company') }})</b></h4>
+      </div>
+      
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -13,9 +19,12 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('messages.dashboard') }}
                     </x-nav-link>
                 </div>
+                <x-nav-link :href="route('language.index')" :active="request()->routeIs('chirps.index')">
+                    {{ __('Language') }}
+                </x-nav-link>
             </div>
 
             <!-- Settings Dropdown -->
@@ -48,34 +57,7 @@
                     </x-slot>
                 </x-dropdown>
             </div>
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
+            
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
@@ -95,26 +77,28 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('language.index')" :active="request()->routeIs('chirps.index')">
+                {{ __('Language') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                @foreach (config('app.locales') as $locale)
+                        <div class="px-4 py-2">
+                            <a href="{{url(getCurrentUrlWithLocale($locale))}}">    
+                               @if(app()->isLocale($locale))
+                               <span>x</span>
+                               @endif
+                                <span> {{$locale}}</span>
+                            </a>  
+                        </div>
+                       @endforeach
             </div>
 
             <div class="mt-3 space-y-1">
                 <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>

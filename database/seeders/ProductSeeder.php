1c1742c590805a88db8442aa2effddf4ab81ecbe
usadilsondigital <usadilsondigital@gmail.com>
2022-11-05 00:10:31 -0500

    Product Seeder Update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('products')->insert([
+            'title' => 'product1',
+            'description' => 'Descript1',
+            'created_at' => now(),
+        ]);
+        DB::table('products')->insert([
+            'title' => 'product2',
+            'description' => 'Descript2',
+            'created_at' => now(),
+        ]);
     }
 }

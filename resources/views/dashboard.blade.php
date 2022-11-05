b43510aec91284943cb0a41d40945fca8563b8d7
usadilsondigital <usadilsondigital@gmail.com>
2022-11-05 00:10:56 -0500

    Blade Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.layout')
  
 @section('current')
-<li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+<li class="breadcrumb-item active" aria-current="page"> {{ __('messages.dashboard') }}</li>
 @endsection
 
 @section('content')
 <div class="card text-center">
     <div class="card-body">
-        <h5 class="card-title"> {{ __('messages.inital') }}</h5>       
+        <h5 class="card-title"> {{ __('messages.dashboard') }}</h5>       
     </div>
 </div>
 @endsection

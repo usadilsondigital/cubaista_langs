fe1c282bbab98408fdae94f8683a18a12b39eda5
usadilsondigital <usadilsondigital@gmail.com>
2022-11-06 17:30:51 -0600

    Control panel COntroller update

--- app/Http/Controllers/ControlpanelController.php
+++ app/Http/Controllers/ControlpanelController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\DB;
+use App\Models\About;
+use App\Models\Language;
+use App\Models\User;
 
 class ControlpanelController extends Controller
 {
-    //
+    public function index()
+    {
+        $abouts = About::all();
+        $languages = Language::all();      
+        $users = User::all();        
+        $codes = Language::pluck('code')->toArray();
+        return view('controlview.index', ['users' => $users,'languages' => $languages,'abouts' => $abouts,'codes' => $codes]);
+    }
 }

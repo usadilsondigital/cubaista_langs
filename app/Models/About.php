2bacf03ff8008cb73e3f3bba388f65c7695fb3a2
usadilsondigital <usadilsondigital@gmail.com>
2022-11-06 17:31:35 -0600

    fillable, translatable about

--- app/Models/About.php
+++ app/Models/About.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class About extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
+    public $translatable = ['title','body'];
 }

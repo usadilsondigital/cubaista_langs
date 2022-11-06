94cf3d12912aeb9f272a9ed52019d9fc5e3c8ecc
usadilsondigital <usadilsondigital@gmail.com>
2022-11-06 06:45:17 -0600

    About Seeder n Route

--- routes/web.php
+++ routes/web.php
@@ -36,24 +36,31 @@ Route::get('/initial', function () {
     $codes = Language::pluck('code')->toArray();
    
     return view('mainview.initial',['codes' => $codes]);
-});
+})->name('home');
 
 /* MODELS */
 Route::controller(LanguageController::class)->group(function () {
     Route::get('/language', 'index')->name('language.index')->middleware(['can:admin','auth']);
     Route::post('/language', 'store')->name('language.store')->middleware(['auth']); 
     Route::get('/languages', 'list')->name('language.list');
-    Route::get('/language/{id}/edit', 'edit')->name('language.edit');
+    Route::get('/language/{id}/edit', 'edit')->name('language.edit')->middleware(['auth']);
 });
 
 Route::controller(ProductController::class)->group(function () {
     Route::get('/product', 'create')->name('product.create')->middleware(['can:admin','auth']);
     Route::post('/product', 'store')->name('product.store')->middleware(['auth']); 
     Route::get('/products', 'index')->name('product.index');
-    Route::get('/product/{id}/edit', 'edit')->name('product.edit');
+    Route::get('/product/{id}/edit', 'edit')->name('product.edit')->middleware(['auth']);
 });
 
-Route::get('/about', function () {    
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about', 'create')->name('about.create')->middleware(['can:admin','auth']);
+    Route::post('/about', 'store')->name('about.store')->middleware(['auth']); 
+    Route::get('/abouts', 'index')->name('about.index');
+    Route::get('/about/{id}/edit', 'edit')->name('about.edit')->middleware(['auth']);
+});
+
+Route::get('/aboutus', function () {    
     $codes = Language::pluck('code')->toArray();    
     return view('aboutview.about', ['codes' => $codes]);
 });

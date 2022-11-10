08b4d2b56aba94a0eb20241c9d04bf01fcf91626
usadilsondigital <usadilsondigital@gmail.com>
2022-11-09 21:35:40 -0600

    Confirmation Mail

---
+++ app/Mail/ConfirmationMail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\DB;
+
+class ConfirmationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $email;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Confirmation Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'mailview.index',
+            with: ['email' => $this->email]
+        );
+        //return $this->view('mailview.index', ['email' => $this->email]);
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}

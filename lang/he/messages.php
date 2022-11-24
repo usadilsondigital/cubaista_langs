6a1088712ed364947aaf87ffdd370ed5f38354bd
usadilsondigital <usadilsondigital@gmail.com>
2022-11-23 20:25:26 -0600

    he	Hebrew

---
+++ lang/he/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages English
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default ones 
+    |
+    */
+
+    'message' => ' message ',
+    'dashboard' => ' dashboard ',
+    'you_love_cuba' => ' You love Cuba? ',
+    'or' => ' Or ',
+    'think_you_might' => ' think you might? ',
+    'so_you_are_a_cubaista' => ' So, you are a Cubaista! ',
+    'and' => ' And ',
+    'we_are_your_dmc' => ' we are your DMC ',
+    'destination_management_company' => 'Destination Management Company',
+
+];

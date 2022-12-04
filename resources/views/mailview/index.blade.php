55ccc4c84c5d363d7c118482d2ba829748b0da32
usadilsondigital <usadilsondigital@gmail.com>
2022-12-04 11:52:50 -0600

    mail view blade

---
+++ resources/views/mailview/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Page Title</title>
+</head>
+<body>
+
+   <a href='{{$email}}'>Confirm your email and Create Password</a>
+               
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
